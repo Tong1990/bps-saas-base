a4452a3bb8041896a4396b04d8b32e22be48a240
yujian <yujian12@countrygarden.com.cn>
2018-08-30 15:10:19 +0800

    init提交

--- bps-saas-base-gateway/src/test/java/com/tony/saas/BpsSaasBaseGatewayApplicationTests.java
+++ bps-saas-base-gateway/src/test/java/com/tony/saas/BpsSaasBaseGatewayApplicationTests.java
@@ -1,15 +1,7 @@
 package com.tony.saas;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class BpsSaasBaseGatewayApplicationTests {
 
-	@Test
 	public void contextLoads() {
 	}
 
